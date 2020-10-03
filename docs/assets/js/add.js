f572e97046e1f61d01b0f21d1b5b8943eed02f51
smilechim8 <67528426+smilechim8@users.noreply.github.com>
2020-10-03 21:22:05 +0900

    up

--- docs/assets/js/add.js
+++ docs/assets/js/add.js
@@ -1,7 +1,7 @@
 $(function() {
 	var topBtn = $('#page_top');topBtn.hide();
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 500) {
             topBtn.fadeIn();
         } else {
             topBtn.fadeOut();
