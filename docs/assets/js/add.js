36dcec8b9bf4d8799079cd71acc8a54415cf824b
smilechim8 <67528426+smilechim8@users.noreply.github.com>
2020-10-03 11:59:39 +0900

    up

---
+++ docs/assets/js/add.js
@@ -0,0 +1,14 @@
+$(function() {
+	var topBtn = $('#page_top');topBtn.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    topBtn.click(function () {
+        $('body,html').animate({scrollTop: 0}, 500);
+        return false;
+    });
+});
